ee7cdac54efc9bf11d3f61cf3e7f470c81786830
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2025-01-20 22:22:01 +0100

    Add integration tests

---
+++ tests/integration/helpers.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+
+import logging
+import subprocess
+from types import SimpleNamespace
+
+from pytest_operator.plugin import OpsTest
+from tenacity import Retrying, stop_after_delay, wait_fixed
+
+logger = logging.getLogger(__name__)
+
+
+K8S_DB_MODEL_NAME = "database"
+MICROK8S_CLOUD_NAME = "cloudk8s"
+
+
+CONFIG_OPTS = {"workload_name": "test_mode", "parallel_processes": 1}
+SERIES = "jammy"
+KAFKA = "kafka"
+APP_NAME = "kafka-benchmark"
+KAFKA_CHANNEL = "3/edge"
+DEFAULT_NUM_UNITS = 2
+
+
+KRAFT_CONFIG = {
+    "profile": "testing",
+    "roles": "broker,controller",
+}
+
+
+MODEL_CONFIG = {
+    "logging-config": "<root>=INFO;unit=DEBUG",
+    "update-status-hook-interval": "1m",
+}
+
+
+def check_service(svc_name: str, unit_id: int = 0, retry_if_fail: bool = True) -> str | None:
+    if not retry_if_fail:
+        return subprocess.check_output(
+            [
+                "juju",
+                "ssh",
+                f"{APP_NAME}/{unit_id}",
+                "--",
+                "sudo",
+                "systemctl",
+                "is-active",
+                svc_name,
+            ],
+            text=True,
+        )
+    for attempt in Retrying(stop=stop_after_delay(150), wait=wait_fixed(15)):
+        with attempt:
+            return subprocess.check_output(
+                [
+                    "juju",
+                    "ssh",
+                    f"{APP_NAME}/{unit_id}",
+                    "--",
+                    "sudo",
+                    "systemctl",
+                    "is-active",
+                    svc_name,
+                ],
+                text=True,
+            )
+
+
+async def run_action(
+    ops_test, action_name: str, unit_name: str, timeout: int = 30, **action_kwargs
+):
+    """Runs the given action on the given unit."""
+    client_unit = ops_test.model.units.get(unit_name)
+    action = await client_unit.run_action(action_name, **action_kwargs)
+    result = await action.wait()
+    logging.info(f"request results: {result.results}")
+    return SimpleNamespace(status=result.status or "completed", response=result.results)
+
+
+async def get_leader_unit_id(ops_test: OpsTest, app: str = APP_NAME) -> int:
+    """Helper function that retrieves the leader unit ID."""
+    leader_unit = None
+    for unit in ops_test.model.applications[app].units:
+        if await unit.is_leader_from_status():
+            leader_unit = unit
+            break
+
+    if not leader_unit:
+        raise ValueError("Leader unit not found")
+
+    return int(leader_unit.name.split("/")[1])
---
+++ tests/integration/test_charm.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+import logging
+
+import pytest
+from pytest_operator.plugin import OpsTest
+
+from .helpers import (
+    APP_NAME,
+    CONFIG_OPTS,
+    DEFAULT_NUM_UNITS,
+    KAFKA,
+    KAFKA_CHANNEL,
+    KRAFT_CONFIG,
+    MODEL_CONFIG,
+    SERIES,
+    check_service,
+    get_leader_unit_id,
+    run_action,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@pytest.mark.parametrize("use_tls", [True, False])
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_deploy(ops_test: OpsTest, kafka_benchmark_charm, use_tls) -> None:
+    """Build and deploy OpenSearch with a single unit and remove it."""
+    await ops_test.model.set_config(MODEL_CONFIG)
+
+    await ops_test.model.deploy(
+        kafka_benchmark_charm,
+        num_units=DEFAULT_NUM_UNITS,
+        series=SERIES,
+        config=CONFIG_OPTS,
+    )
+    await ops_test.model.deploy(
+        KAFKA,
+        channel=KAFKA_CHANNEL,
+        config=KRAFT_CONFIG,
+        num_units=DEFAULT_NUM_UNITS,
+        series=SERIES,
+    )
+    await ops_test.model.integrate(KAFKA, APP_NAME)
+    if use_tls:
+        await ops_test.model.deploy(
+            "self-signed-certificates",
+            num_units=1,
+            series=SERIES,
+        )
+        await ops_test.model.integrate(KAFKA, "self-signed-certificates")
+        await ops_test.model.integrate(APP_NAME, "self-signed-certificates")
+
+    await ops_test.model.wait_for_idle(apps=[KAFKA], status="active", timeout=1000)
+    await ops_test.model.wait_for_idle(apps=[APP_NAME], status="waiting", timeout=1000)
+
+    assert len(ops_test.model.applications[APP_NAME].units) == DEFAULT_NUM_UNITS
+
+
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_prepare(ops_test: OpsTest) -> None:
+    # async def test_prepare(ops_test: OpsTest, kafka_benchmark_charm) -> None:
+    """Build and deploy OpenSearch with a single unit and remove it."""
+    leader_id = await get_leader_unit_id(ops_test)
+
+    output = await run_action(ops_test, "prepare", f"{APP_NAME}/{leader_id}")
+    assert output.status == "completed"
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="waiting",
+        raise_on_blocked=True,
+        timeout=15 * 60,
+    )
+
+
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_run(ops_test: OpsTest) -> None:
+    # async def test_prepare(ops_test: OpsTest, kafka_benchmark_charm) -> None:
+    """Build and deploy OpenSearch with a single unit and remove it."""
+    leader_id = await get_leader_unit_id(ops_test)
+
+    output = await run_action(ops_test, "run", f"{APP_NAME}/{leader_id}")
+    assert output.status == "completed"
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        raise_on_blocked=True,
+        timeout=15 * 60,
+    )
+
+
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_stop(ops_test: OpsTest) -> None:
+    # async def test_prepare(ops_test: OpsTest, kafka_benchmark_charm) -> None:
+    """Build and deploy OpenSearch with a single unit and remove it."""
+    leader_id = await get_leader_unit_id(ops_test)
+
+    output = await run_action(ops_test, "stop", f"{APP_NAME}/{leader_id}")
+    assert output.status == "completed"
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="waiting",
+        raise_on_blocked=True,
+        timeout=15 * 60,
+    )
+
+
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_restart(ops_test: OpsTest) -> None:
+    # async def test_prepare(ops_test: OpsTest, kafka_benchmark_charm) -> None:
+    """Build and deploy OpenSearch with a single unit and remove it."""
+    leader_id = await get_leader_unit_id(ops_test)
+
+    output = await run_action(ops_test, "run", f"{APP_NAME}/{leader_id}")
+    assert output.status == "completed"
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        raise_on_blocked=True,
+        timeout=15 * 60,
+    )
+    assert check_service("dpe_benchmark", unit_id=leader_id)
+
+
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_clean(ops_test: OpsTest) -> None:
+    # async def test_prepare(ops_test: OpsTest, kafka_benchmark_charm) -> None:
+    """Build and deploy OpenSearch with a single unit and remove it."""
+    leader_id = await get_leader_unit_id(ops_test)
+
+    output = await run_action(ops_test, "cleanup", f"{APP_NAME}/{leader_id}")
+    assert output.status == "completed"
+
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME],
+        status="active",
+        raise_on_blocked=True,
+        timeout=15 * 60,
+    )
+    assert check_service("dpe_benchmark", unit_id=leader_id)
