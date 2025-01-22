69f7982ae18745e1049b91c806852aeef8f8e472
phvalguima <pedro.guimaraes@canonical.com>
2025-01-22 20:16:13 +0100

    [DPE-6296] Pyright fixes + structured_config additions + break down of actions.py (#13)

--- src/benchmark/core/models.py
+++ src/benchmark/core/models.py
@@ -9,7 +9,7 @@ as changes in the configuration.
 """
 
 import logging
-from typing import Any, Optional
+from typing import Any, MutableMapping, Optional
 
 from ops.model import Application, Relation, Unit
 from overrides import override
@@ -17,7 +17,6 @@ from pydantic import BaseModel, error_wrappers, root_validator
 
 from benchmark.literals import (
     LIFECYCLE_KEY,
-    STOP_KEY,
     DPBenchmarkLifecycleState,
     DPBenchmarkMissingOptionsError,
     Scope,
@@ -106,7 +105,6 @@ class DPBenchmarkWrapperOptionsModel(BaseModel):
     workload_name: str
     db_info: DPBenchmarkBaseDatabaseModel
     report_interval: int
-    workload_profile: str
     labels: str
     peers: str | None = None
 
@@ -125,20 +123,18 @@ class RelationState:
         self.scope = scope
 
     @property
-    def relation_data(self) -> dict[str, str]:
+    def relation_data(self) -> MutableMapping[str, str]:
         """Returns the relation data."""
         if self.relation:
             return self.relation.data[self.component]
         return {}
 
     @property
-    def remote_data(self) -> dict[str, str]:
+    def remote_data(self) -> MutableMapping[str, str]:
         """Returns the remote relation data."""
-        if not self.relation:
+        if not self.relation or self.scope != Scope.APP:
             return {}
-        if self.scope == Scope.APP:
-            return self.relation.data[self.relation.app]
-        return self.relation.data[self.relation.unit]
+        return self.relation.data[self.relation.app]
 
     def __bool__(self) -> bool:
         """Boolean evaluation based on the existence of self.relation."""
@@ -191,16 +187,6 @@ class PeerState(RelationState):
         else:
             self.set({LIFECYCLE_KEY: status})
 
-    @property
-    def stop(self) -> bool:
-        """Returns the value of the stop key."""
-        return self.relation_data.get(STOP_KEY, False)
-
-    @stop.setter
-    def stop(self, switch: bool) -> bool:
-        """Toggles the stop key value."""
-        self.set({STOP_KEY: switch})
-
 
 class DatabaseState(RelationState):
     """State collection for the database relation."""
@@ -236,7 +222,7 @@ class DatabaseState(RelationState):
             return None
         return tls_ca
 
-    def get(self) -> DPBenchmarkBaseDatabaseModel | None:
+    def model(self) -> DPBenchmarkBaseDatabaseModel | None:
         """Returns the value of the key."""
         if not self.relation or not (endpoints := self.remote_data.get("endpoints")):
             return None
@@ -248,9 +234,9 @@ class DatabaseState(RelationState):
             return DPBenchmarkBaseDatabaseModel(
                 hosts=endpoints.split(),
                 unix_socket=unix_socket,
-                username=self.data.get("username"),
-                password=self.data.get("password"),
-                db_name=self.remote_data.get(self.database_key),
+                username=self.data.get("username", ""),
+                password=self.data.get("password", ""),
+                db_name=self.remote_data.get(self.database_key, ""),
                 tls=self.tls,
                 tls_ca=self.tls_ca,
             )
